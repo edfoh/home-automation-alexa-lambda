af0da0872906a4d6059754cbd96dec9b3f02ceb2
efoh <efoh@seek.com.au>
2017-05-26 20:50:32 +1000

    Added more commands for chromecast

--- src/chromecastIntents.py
+++ src/chromecastIntents.py
@@ -15,12 +15,14 @@ def post_request(path):
 
     print("making POST request to url {}".format(url))
     response = requests.post(url, timeout=30, auth=HTTPBasicAuth(api_username, api_password))
-    if (response.status_code == 200):
+    if response.status_code == 200:
         print('request successful!')
-        return True
+        return True, None
+    elif repsonse.status_code == 400:
+        return False, response.json()['message']
     else:
         print("url {} post failed with status code {}".format(url, response.status_code))
-        return False
+        return False, None
 
 def get_request(path):
     host = os.environ.get("HOST_ADDRESS")
@@ -62,7 +64,7 @@ def chromecast_stop(slots):
     print card_title
     sys.stdout.flush()
 
-    success = post_request('chromecast/stop')
+    success, err_message = post_request('chromecast/stop')
     answer = 'ok' if success else 'could not stop'
     return build_alexa_response(answer, card_title)
 
@@ -76,11 +78,61 @@ def chromecast_play_first_video_in_playlist(slots):
     answer = 'Playing playlist {}'.format(playlist_name) if success else 'could not play playlist {}'.format(playlist_name)
     return build_alexa_response(answer, card_title)
 
+def chromecast_pause(slots):
+    card_title = 'Chromecast Pause'
+    print card_title
+    sys.stdout.flush()
+
+    success, err_message = post_request('chromecast/pause')
+    answer = 'ok' if success else err_message
+    return build_alexa_response(answer, card_title)
+
+def chromecast_play(slots):
+    card_title = 'Chromecast Play'
+    print card_title
+    sys.stdout.flush()
+
+    success, err_message = post_request('chromecast/play')
+    answer = 'ok' if success else err_message
+    return build_alexa_response(answer, card_title)
+
+def chromecast_resume(slots):
+    card_title = 'Chromecast Resume'
+    print card_title
+    sys.stdout.flush()
+
+    success, err_message = post_request('chromecast/resume')
+    answer = 'ok' if success else err_message
+    return build_alexa_response(answer, card_title)
+
+def chromecast_play_next(slots):
+    card_title = 'Chromecast Play Next'
+    print card_title
+    sys.stdout.flush()
+
+    success, err_message = post_request('chromecast/play/next')
+    answer = 'ok' if success else err_message
+    return build_alexa_response(answer, card_title)
+
+def chromecast_play_previous(slots):
+    card_title = 'Chromecast Play Previous'
+    print card_title
+    sys.stdout.flush()
+
+    success, err_message = post_request('chromecast/play/previous')
+    answer = 'ok' if success else err_message
+    return build_alexa_response(answer, card_title)
+
 INTENTS = {
     'ChromecastTest': chromecast_test,
     'ChromecastGetPlaylist': chromecast_get_playlist,
     'ChromecastPlayFirstVideoInPlaylist': chromecast_play_first_video_in_playlist,
-    'ChromecastStop': chromecast_stop
+    'ChromecastStop': chromecast_stop,
+    'ChromecastPause': chromecast_pause,
+    'ChromecastPlay': chromecast_play,
+    'ChromecastResume': chromecast_resume,
+    'ChromecastPlayNext': chromecast_play_next,
+    'ChromecastPlayPrevious': chromecast_play_previous
 }
 
 def on_chromecast_intent(intent_request, session):
